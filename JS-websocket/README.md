1b342e60888c50dbbcc2cb12484f5d06ef2b3b58
Wee Yuanliang <quaaludes81@hotmail.com>
2022-09-28 17:42:28 +0800

    update

--- JS-websocket/README.md
+++ JS-websocket/README.md
@@ -3,4 +3,7 @@ npm install -g wscat
 ### for testing endpoint
 npm install -g wscat 
 - To connect > wscat -c wss://ioowgj15h0.execute-api.us-east-1.amazonaws.com/dev
-{"action":"sendMessage", "data","hello"}
+{"action":"sendMessage", "data":"hello"}
+
+### AWS
+attach policy to lambdaRole and redeploy to work: AmazonAPIGatewayInvokeFullAccess
--- JS-websocket/handler.js
+++ JS-websocket/handler.js
@@ -1,4 +1,5 @@
 "use strict";
+const AWS = require("aws-sdk");
 
 const successfullResponse = {
   statusCode: 200,
@@ -6,8 +7,46 @@ const successfullResponse = {
 };
 
 module.exports.connectionHandler = (event, context, callback) => {
-  console.log(`event: ${ JSON.stringify(event)}`)
   console.log(`context: ${JSON.stringify(context)}`)
   // Handle connection
-  callback(null, successfullResponse) // for connection in pie socket / wscat
+  // NOTE: To simulate client server and to see event, connect in pie socket
+  try {
+    const connectionId = event.requestContext.connectionId
+    console.log(`connectionId: ${connectionId}`)
+    callback(null, successfullResponse) // for connection in pie socket / wscat
+  } catch (error) {
+    console.log(error)
+  }
+
 }
+
+module.exports.sendMessageHandler = (event, context, callback) => {
+
+  const connectionId = event.requestContext.connectionId
+  console.log(`connectionId: ${connectionId}`)
+  send(event, connectionId)
+
+  callback(null, successfullResponse) 
+}
+
+const send = (event, connectionId) => {
+
+  // get from pie socket action / data
+  const body = JSON.parse(event.body);
+  const postData = body.data;
+  console.log(postData)
+
+  // set aws api backend point and client connectionId 
+  const endpoint = event.requestContext.domainName + "/" + event.requestContext.stage;
+  console.log(`backend endpt: ${endpoint}`)
+  const apigwManagementApi = new AWS.ApiGatewayManagementApi({
+    endpoint: endpoint,
+  });
+  const params = {
+    ConnectionId: connectionId,
+    Data: postData,
+  };
+
+  // post back to client
+  return apigwManagementApi.postToConnection(params).promise();
+};
