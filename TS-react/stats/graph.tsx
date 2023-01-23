273b15111ffe19cf30cc21c6076350c42cf4b5ef
leon_wee <leon_wee@techpass.gov.sg>
2023-01-23 13:07:43 +0800

    stats

---
+++ TS-react/stats/graph.tsx
@@ -0,0 +1,32 @@
+// BAR
+export const bar_options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart',
+    },
+  },
+};
+
+// STACKED 
+export const stack_options = {
+  plugins: {
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart - Stacked',
+    },
+  },
+  responsive: true,
+  scales: {
+    x: {
+      stacked: true,
+    },
+    y: {
+      stacked: true,
+    },
+  },
+};
