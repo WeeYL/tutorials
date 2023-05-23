aff629f80bd251c38b1dbca04d20d47f3d68e290
Wee Yuanliang <quaaludes81@hotmail.com>
2023-05-23 16:05:15 +0800

    rn

--- TS-ReactNative-Basic/screens/DetailScreen.tsx
+++ TS-ReactNative-Basic/screens/DetailScreen.tsx
@@ -1,14 +1,47 @@
 import { NativeStackHeaderProps } from "@react-navigation/native-stack";
-import { Button, FlatList, Pressable, StyleSheet, Text, View } from "react-native";
-import {styles} from '../styles/styles'
+import {
+  Button,
+  FlatList,
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { styles } from "../styles/styles";
 
-export function DetailScreen({route}:any) {
-  const { itemId, otherParam } = route.params
-    return (
-        <View style={styles.container}>
-        <Text>Detail Screen</Text>
-        <Text> Id: {itemId}</Text>
-        <Text> otherParam: {otherParam}</Text>
-      </View>
-    );
-  }
+export function DetailScreen({ route }: any) {
+  const { itemId, otherParam, flatListItemFromHome } = route.params;
+  console.log(flatListItemFromHome)
+  const flatListItem = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    },
+  ];
+
+  return (
+      <SafeAreaView style={styles.container} >
+
+      <Text>Detail Screen</Text>
+      <Text> Id: {itemId}</Text>
+      <Text> otherParam: {otherParam}</Text>
+
+      <FlatList
+        data={flatListItem}
+        renderItem={({item}) => <Button title={item.title} />}
+        keyExtractor={item => item.id}
+      />
+
+
+    </SafeAreaView>
+  );
+}
--- TS-ReactNative-Basic/screens/HomeScreen.tsx
+++ TS-ReactNative-Basic/screens/HomeScreen.tsx
@@ -1,23 +1,41 @@
 import { Text, View } from "react-native";
-import { styles } from '../styles/styles';
+import { styles } from "../styles/styles";
 import { PressableBtn } from "../component";
 
-export function HomeScreen({navigation}:any) {
+export function HomeScreen({ navigation }: any) {
+  const detailList = {
+    itemId: "123",
+    otherParam: "Toys",
+  };
 
-    const detailList = {
-        itemId:"123",
-        otherParam:"Toys"
-    }
+  const flatListItemFromHome = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    },
+  ];
 
-    return (
-      <View style={styles.container}>
-        <Text>Home Screen</Text>
-        <PressableBtn title="Detail !"
-        onPress={()=>navigation.navigate("Detail" , detailList) // pass params to route
-        }/>
-        <PressableBtn title="Timer !"
-        onPress={()=>navigation.navigate("Timer")} // user Effect
-        /> 
-      </View>
-    );
-  }
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+      <PressableBtn
+        title="Detail !"
+        onPress={
+          () => navigation.navigate("Detail", detailList, flatListItemFromHome) // pass params to route
+        }
+      />
+      <PressableBtn
+        title="Timer !"
+        onPress={() => navigation.navigate("Timer")} // user Effect
+      />
+    </View>
+  );
+}
