0d029fedc80699174c968a15bd076c7164da0464
Wee Yuanliang <quaaludes81@hotmail.com>
2023-07-17 22:25:39 +0800

    css

--- JS-CSS-1/JS/script.js
+++ JS-CSS-1/JS/script.js
@@ -1,1 +1,13 @@
-console.log("hello")
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+
+btnHamburger.addEventListener('click', function(){
+    
+    if (header.classList.contains("open")){
+        header.classList.remove("open")
+    } else {
+        console.log("ham")
+        header.classList.add("open")
+    }
+})
+
