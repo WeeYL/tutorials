ddc492e0656d251b88e4003685a328d13409e052
Wee Yuanliang <quaaludes81@hotmail.com>
2023-07-17 13:48:24 +0800

    bank

--- SCSS/readme.md
+++ SCSS/readme.md
@@ -30,6 +30,7 @@
 |* | all elements
 | div | all div tags
 | div,p | all divs and paragraphs
+| p.intro | elements with class="intro"
 | div p | paragraphs inside divs
 | div > p | all p tags, one level deep in div
 | div + p | p tags immediately after div
--- pcweb4-ejs/server.js
+++ pcweb4-ejs/server.js
@@ -22,82 +22,77 @@ async function getPlayers() {
 }
 
 async function getPlayer(id) {
-    const [rows] = await pool.query(
-        `
+  const [rows] = await pool.query(
+    `
         SELECT * FROM players
         WHERE id = ?
         `,
-        [id]
-    );
-    return rows;
+    [id]
+  );
+  return rows;
 }
-
 
 async function addPlayer(name, description, image) {
-    const [result] = await pool.query(
-        `INSERT INTO players (name, description, image)
+  const [result] = await pool.query(
+    `INSERT INTO players (name, description, image)
          VALUES (?, ?, ?)`,
-         [name, description, image]
-    )
-    const id = result.insertId;
-    return getPlayer(id), result
+    [name, description, image]
+  );
+  const id = result.insertId;
+  return getPlayer(id), result;
 }
 
-async function updatePlayer(name, description, image,id) {
-    const [result] = await pool.query (
-        `
+async function updatePlayer(name, description, image, id) {
+  const [result] = await pool.query(
+    `
         UPDATE players
         SET name=?, description=?, image=?
         WHERE id=? 
         `,
-        [name, description, image,id]
-    )
+    [name, description, image, id]
+  );
 }
 
 async function deletePlayer(id) {
-    const [result] = await pool.query(
-        `DELETE FROM players WHERE id = ?`,[id]
-    )
-
+  const [result] = await pool.query(`DELETE FROM players WHERE id = ?`, [id]);
 }
 
 app.listen(port, () => {
   console.log(`listening to port ${port}`);
 });
-app.get("/add",async (req,res)=>{
-    res.render("add.ejs")
-})
+app.get("/add", async (req, res) => {
+  res.render("add.ejs");
+});
 
-app.post("/add",async (req,res)=>{
-    const {name, description, image} = req.body
-    const note = await addPlayer(name, description, image)
-    console.log(note)
-    res.redirect("/");
-})
-
+app.post("/add", async (req, res) => {
+  const { name, description, image } = req.body;
+  const note = await addPlayer(name, description, image);
+  console.log(note);
+  res.redirect("/");
+});
 
 app.get("/", async (req, res) => {
   const players = await getPlayers();
   res.render("index.ejs", { players });
 });
 
-app.get("/delete/:id", async (req, res) =>{
-    const id = req.params.id 
-    await deletePlayer(id)
-    res.redirect("/")
-})
+app.get("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  await deletePlayer(id);
+  res.redirect("/");
+});
 
-app.post("/update/:id",async (req,res)=>{
-    const id = req.params.id
-    const {name, description, image} = req.body
-    console.log(name,description)
-    console.log(`updated: ${player}`)
-    await updatePlayer(name, description, image, id)
-    res.redirect("/")
-})
-app.get("/update/:id",async (req,res)=>{
-    const id = req.params.id
-    const player = await getPlayer(id)
+app.post("/update/:id", async (req, res) => {
+  const id = req.params.id;
+  const { name, description, image } = req.body;
+  console.log(name, description);
+  console.log(`updated: ${player}`);
+  await updatePlayer(name, description, image, id);
+  res.redirect("/");
+});
+app.get("/update/:id", async (req, res) => {
+  const id = req.params.id;
+  const player = await getPlayer(id);
 
-    res.render("update.ejs",{player:player[0]})
-})
+  res.render("update.ejs", { player: player[0] }); // render ejs and pass object
+});
