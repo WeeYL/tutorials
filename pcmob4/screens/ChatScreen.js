2c67f1224a32c46ddb5065a9c9a43cbfb07b1c0c
Wee Yuanliang <quaaludes81@hotmail.com>
2023-09-08 16:49:07 +0800

    test

--- pcmob4/screens/ChatScreen.js
+++ pcmob4/screens/ChatScreen.js
@@ -1,8 +1,10 @@
-import React, { useEffect } from "react";
-import { Button, View } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
-import { auth } from "../firebase";
 import { onAuthStateChanged, signOut } from "firebase/auth";
+import React, { useEffect } from "react";
+import { View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { auth } from "../firebase";
 
 export default function ChatScreen() {
   const navigation = useNavigation();
@@ -12,13 +14,17 @@ export default function ChatScreen() {
       if (user) navigation.navigate("Chat", { id: user.id, email: user.email });
       else navigation.navigate("Login");
     });
+
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={logout}>
+          <MaterialCommunityIcons name="logout" size={30} color="grey" />
+        </TouchableOpacity>
+      ),
+    });
   }, []);
 
   const logout = () => signOut(auth);
 
-  return (
-    <View>
-      <Button onPress={logout} title="Logout" />
-    </View>
-  );
+  return <View></View>;
 }
